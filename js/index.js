e17300d804c8b58b0cf531192862c142ffc507e1
DJGValls <jiga1980@gmail.com>
2023-01-17 18:20:24 +0100

    second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
-// Iteration 1: Names and Input
+console.log("Hellow world")
 
+// Iteration 1: Names and Input
+let hacker1 = "daniel";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "chrome";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}else if(hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}else if(hacker1.length === hacker2.length){
+    console.log(`Wow, you both have equally long names, it has ${hacker1.length} characters!`)
+}
 
 // Iteration 3: Loops
+let spaced = "";
+for(let i=0; i<hacker1.length; i++){
+    spaced += hacker1[i] + " ";
+}
+console.log(spaced.toUpperCase())
+
+let spaced2 = "";
+for(let i=hacker1.length; i>=0; i--){
+    spaced2 += hacker1[i] + " ";
+}
+console.log(spaced2.toUpperCase())
