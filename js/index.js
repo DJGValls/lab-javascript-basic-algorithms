844853f3cc444b5fb60dc8777359dd0ea1d25830
DJGValls <jiga1980@gmail.com>
2023-01-17 19:03:04 +0100

    finish commit

--- js/index.js
+++ js/index.js
@@ -16,14 +16,34 @@ if(hacker1.length > hacker2.length){
 }
 
 // Iteration 3: Loops
+
+//3.1
 let spaced = "";
 for(let i=0; i<hacker1.length; i++){
     spaced += hacker1[i] + " ";
 }
 console.log(spaced.toUpperCase())
 
-let spaced2 = "";
-for(let i=hacker1.length; i>=0; i--){
-    spaced2 += hacker1[i] + " ";
+//3.2
+let nameVariable = "";
+for(let i=hacker1.length - 1; i>=0; i--){
+    nameVariable += hacker1[i];
+    //console.log(hacker1[i]);
 }
-console.log(spaced2.toUpperCase())
+console.log(nameVariable);
+
+//3.3
+for(let i=0; i<hacker1.length; i++){
+    if(hacker1 === hacker2){
+        console.log("What?! You both have the same name?");
+        break;
+     }else if(hacker1[i] < hacker2[i]){
+         console.log("The driver's name goes first.");
+         break;
+      }else{
+         console.log("Yo, the navigator goes first definitely.");
+         break;
+      }
+}
+
+
